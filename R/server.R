5f1ce04f79ae04ee658d45a0afe05ab0a85f64e9
bluefoxr <william.becker@bluefoxdata.eu>
2023-04-14 16:35:36 +0200

    enable add/remove, enhance and fix bugs

--- R/server.R
+++ R/server.R
@@ -1,9 +1,13 @@
 app_server <- function(input, output, session) {
 
+  # initialise reactives to be shared between modules. These can be modified
+  # within any module and don't need to be passed back out.
+  coin <- reactiveVal(NULL)
+  coin_full <- reactiveVal(NULL)
 
-  coin <- input_server("id_input")
+  input_server("id_input", coin, coin_full)
 
-  analysis_server("id_analysis", coin, input)
+  analysis_server("id_analysis", coin, coin_full, input)
 
 
 
