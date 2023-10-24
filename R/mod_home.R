4c60a66576d14f1e60f16ac7add42ec2276315bb
Edouard <edouard.legoupil@gmail.com>
2023-10-24 12:07:27 -0500

    add reference to toolkit

--- R/mod_home.R
+++ R/mod_home.R
@@ -28,16 +28,23 @@ mod_home_ui <- function(id) {
 		    " compliant data collection platform.",
 		    style = "font-size: 20px"),
 		  br(),
-
-		  p("This app help configuring your data analysis plan within the
+		  p( "This ",tags$span("companion app", style = "color:#00B398"), " helps configuring your data analysis plan within the
 		      original XlsForm that has been used to collect your dataset.
 		      The original xlsform is extended with additional columns to record your
-		      analysis settings."),
-		  br(),
-		  p("The advantage of this approach is that most processing is de-facto documented.
+		      analysis settings. The advantage of this approach is that most processing is de-facto documented.
 		    The processing includes: Relabelling, grouping questions, setting crosstabulation,
-		      Searching for statistical association, Indicators building"),
+		      Searching for statistical association, Indicators building" ,
+		     style = "font-size: 18px; text-align: left;"),
+
 		  br(),
+		  p("This app is part of a ",tags$span("comprehensive app toolkit", style = "color:#00B398"),
+		    " to mainstream knowledge & enhance the process of survey implementation through Kobotoolbox. It includes: ",
+		    tags$a(href="https://rstudio.unhcr.org/rmsSampling/", "rmsSampling"), " to help designing sampling strategies, ",
+		    tags$a(href="https://rstudio.unhcr.org/Survey_Designer", "SurveyDesigner"), " to help integrating annual survey needs, ",
+		    tags$a(href="https://rstudio.unhcr.org/XlsFormUtil/", "XlsFormUtil"), " to help reviewing form contextualisation, ",
+		    tags$a(href="https://rstudio.unhcr.org/HighFrequencyChecks/", "HighFrequencyChecks"), " to monitor data collection quality, ",
+		    tags$a(href="https://rstudio.unhcr.org/kobocruncher/", "KoboCruncher"), " to perform rapid data exploration and compile indicators.",
+		    style = "font-size: 12px; text-align: left;"),
 		  p(tags$i( class = "fa fa-github"),
 		    "App built with ",
 		    tags$a(href="https://edouard-legoupil.github.io/graveler/",
